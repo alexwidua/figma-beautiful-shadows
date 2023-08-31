36c6d65d0c8a32e53cdaba03bf89de4e3af9f550
Alex Widua <a.widua@gmail.com>
2023-08-30 23:03:09 -0700

    misc

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,52 +1,52 @@
-import { ShadowType } from '../store/createShadowProps'
+import { ShadowType } from "../store/createShadowProps";
 
 /**
  * Plugin window
  */
 
-export const WINDOW_INITIAL_WIDTH = 375
-export const WINDOW_INITIAL_HEIGHT = 375
-export const WINDOW_MIN_WIDTH = 340
-export const WINDOW_MIN_HEIGHT = 340
-export const WINDOW_MAX_WIDTH = 900
-export const WINDOW_MAX_HEIGHT = 900
+export const WINDOW_INITIAL_WIDTH = 375;
+export const WINDOW_INITIAL_HEIGHT = 375;
+export const WINDOW_MIN_WIDTH = 340;
+export const WINDOW_MIN_HEIGHT = 340;
+export const WINDOW_MAX_WIDTH = 900;
+export const WINDOW_MAX_HEIGHT = 900;
 
 /**
  * Performance
  */
-export const DEBOUNCE_CANVAS_UPDATES = 10 // ms
+export const DEBOUNCE_CANVAS_UPDATES = 8; // ms
 
 /**
  * Light source
  */
-export const LIGHT_SIZE = 32
+export const LIGHT_SIZE = 32;
 export const LIGHT_INITIAL_POSITION = {
-	x: WINDOW_INITIAL_WIDTH / 2 - LIGHT_SIZE / 2,
-	y: WINDOW_INITIAL_WIDTH / 2 - 148
-}
-export const LIGHT_INITIAL_BRIGHTNESS = 0.1 // value between 0-1
-export const LIGHT_MIN_BRIGHTNESS = 0.01 // value between 0-1
+  x: WINDOW_INITIAL_WIDTH / 2 - LIGHT_SIZE / 2,
+  y: WINDOW_INITIAL_WIDTH / 2 - 148,
+};
+export const LIGHT_INITIAL_BRIGHTNESS = 0.1; // value between 0-1
+export const LIGHT_MIN_BRIGHTNESS = 0.01; // value between 0-1
 
 /**
  * Target element which casts the shadow
  */
-export const TARGET_INITIAL_ELEVATION = 0.15
+export const TARGET_INITIAL_ELEVATION = 0.15;
 
 /**
  * Background
  */
-export const BACKGROUND_DEFAULT_COLOR = '#e5e5e5'
+export const BACKGROUND_DEFAULT_COLOR = "#e5e5e5";
 
 /**
  * Shadow
  */
-export const SHADOW_DEFAULT_COLOR = '000000'
-export const SHADOW_DEFAULT_TYPE: ShadowType = 'DROP_SHADOW'
-export const SHADOW_BASE_BLUR = 50
+export const SHADOW_DEFAULT_COLOR = "000000";
+export const SHADOW_DEFAULT_TYPE: ShadowType = "DROP_SHADOW";
+export const SHADOW_BASE_BLUR = 50;
 
 /**
  * Preview
  */
-export const LIGHT_SNAP_TO_AXIS_TRESHOLD = 6 //px
-export const GRID_SIZE = 32
-export const GRID_OPACITY = 0.25
+export const LIGHT_SNAP_TO_AXIS_TRESHOLD = 6; //px
+export const GRID_SIZE = 32;
+export const GRID_OPACITY = 0.25;
--- src/hooks/usePreviewBounds.ts
+++ src/hooks/usePreviewBounds.ts
@@ -1,46 +1,43 @@
-import { useEffect, useLayoutEffect, useState } from 'preact/hooks'
-import useDebounce from './useDebounce'
-import { WINDOW_INITIAL_WIDTH, WINDOW_INITIAL_HEIGHT } from '../constants'
+import { useEffect, useLayoutEffect, useState } from "preact/hooks";
+import useDebounce from "./useDebounce";
+import { WINDOW_INITIAL_WIDTH, WINDOW_INITIAL_HEIGHT } from "../constants";
 
 export interface PreviewBounds {
-	width: number
-	height: number
+  width: number;
+  height: number;
 }
 
-const DEBOUNCE_VALUE = 200
+const DEBOUNCE_VALUE = 200;
 
 const usePreviewBounds = (ref: any): PreviewBounds => {
-	const [previewBounds, setPreviewBounds] = useState<PreviewBounds>({
-		width: WINDOW_INITIAL_WIDTH,
-		height: WINDOW_INITIAL_HEIGHT
-	})
-	const debouncedValue = useDebounce<PreviewBounds>(
-		previewBounds,
-		DEBOUNCE_VALUE
-	)
+  const [previewBounds, setPreviewBounds] = useState<PreviewBounds>({
+    width: WINDOW_INITIAL_WIDTH,
+    height: WINDOW_INITIAL_HEIGHT,
+  });
+  const debouncedValue = useDebounce<PreviewBounds>(previewBounds, DEBOUNCE_VALUE);
 
-	const getPreviewBounds = () => {
-		if (!ref.current) return
-		const rect = ref.current.getBoundingClientRect()
+  const getPreviewBounds = () => {
+    if (!ref.current) return;
+    const rect = ref.current.getBoundingClientRect();
 
-		setPreviewBounds({
-			width: rect.width,
-			height: rect.height
-		})
-	}
+    setPreviewBounds({
+      width: rect.width,
+      height: rect.height,
+    });
+  };
 
-	useLayoutEffect(() => {
-		getPreviewBounds()
-	}, [])
+  useLayoutEffect(() => {
+    getPreviewBounds();
+  }, []);
 
-	useEffect(() => {
-		window.addEventListener('resize', getPreviewBounds)
-		return () => {
-			window.removeEventListener('resize', getPreviewBounds)
-		}
-	}, [])
+  useEffect(() => {
+    window.addEventListener("resize", getPreviewBounds);
+    return () => {
+      window.removeEventListener("resize", getPreviewBounds);
+    };
+  }, []);
 
-	return debouncedValue
-}
+  return debouncedValue;
+};
 
-export default usePreviewBounds
+export default usePreviewBounds;
