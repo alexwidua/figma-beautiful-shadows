6f7e1394314fa0562244dcf75c71db019ca6ea42
Alex Widua <a.widua@gmail.com>
2023-08-30 21:28:00 -0700

    fix: #2

--- src/utils/shadow.ts
+++ src/utils/shadow.ts
@@ -7,85 +7,76 @@
  * read about designing shadows in CSS: https://www.joshwcomeau.com/css/designing-shadows/
  */
 
-import { normalize, round, degreeToRadian } from './math'
-import { easeQuadOut, easeQuadIn, easeCubicInOut } from 'd3-ease'
-import { SHADOW_BASE_BLUR } from '../constants'
-import { ShadowType } from '../store/createShadowProps'
+import { normalize, round, degreeToRadian } from "./math";
+import { easeQuadOut, easeQuadIn, easeCubicInOut } from "d3-ease";
+import { SHADOW_BASE_BLUR } from "../constants";
+import { ShadowType } from "../store/createShadowProps";
 
 interface ShadowParameter {
-	numShadows: number
-	azimuth: number
-	distance: number
-	elevation: number
-	brightness: number
-	color: RGBA
-	shadowType: ShadowType
-	size: { width: number; height: number }
+  numShadows: number;
+  azimuth: number;
+  distance: number;
+  elevation: number;
+  brightness: number;
+  color: RGBA;
+  shadowType: ShadowType;
+  size: { width: number; height: number };
 }
 
 export function getCastedShadows({
-	numShadows,
-	azimuth,
-	distance,
-	elevation,
-	brightness,
-	color,
-	shadowType,
-	size
+  numShadows,
+  azimuth,
+  distance,
+  elevation,
+  brightness,
+  color,
+  shadowType,
+  size,
 }: ShadowParameter): DropShadowEffect[] | InnerShadowEffect[] {
-	const azimuthRad = degreeToRadian(azimuth)
+  const azimuthRad = degreeToRadian(azimuth);
 
-	/**
-	 * Match shadow between UI and canvas.
-	 * The UI element is always 100*100px, whereas the canvas element size is indefinite.
-	 * To create the same shadow offset between the UI and canvas layer, we need to scale the offset respectively.
-	 */
-	const { width, height } = size
-	const longestSide = Math.max(width, height)
-	const factor = longestSide / 100 // ui element is 100*100 by default
-	const scale = distance * factor // 20 -> arbitrary value that felt good
+  /**
+   * Match shadow between UI and canvas.
+   * The UI element is always 100*100px, whereas the canvas element size is indefinite.
+   * To create the same shadow offset between the UI and canvas layer, we need to scale the offset respectively.
+   */
+  const { width, height } = size;
+  const longestSide = Math.max(width, height);
+  const factor = longestSide / 100; // ui element is 100*100 by default
+  const scale = distance * factor; // 20 -> arbitrary value that felt good
 
-	const increaseRadiusWithDistance = Math.max(scale / 100, 0.1) // values are purely based on gut feel
+  const increaseRadiusWithDistance = Math.max(scale / 100, 0.1); // values are purely based on gut feel
 
-	const shadows: DropShadowEffect[] | InnerShadowEffect[] = Array.from(
-		{ length: numShadows },
-		(_, i) => {
-			const type: any = shadowType
-			// opacity/ shadow alpha
-			const easeOpacity = easeCubicInOut(normalize(i, 0, numShadows))
-			const a = round(brightness - brightness * easeOpacity, 2)
+  const shadows: DropShadowEffect[] | InnerShadowEffect[] = Array.from({ length: numShadows }, (_, i) => {
+    const type: any = shadowType;
+    // opacity/ shadow alpha
+    const easeOpacity = easeCubicInOut(normalize(i, 0, numShadows));
+    const a = round(brightness - brightness * easeOpacity, 2);
 
-			// offset
-			const easeOffset = easeQuadIn(normalize(i, 0, numShadows)) * 5
-			const x = round(
-				Math.cos(azimuthRad) * (scale * elevation * easeOffset)
-			)
-			const y = round(
-				Math.sin(azimuthRad) * (scale * elevation * easeOffset)
-			)
+    // offset
+    const easeOffset = easeQuadIn(normalize(i, 0, numShadows)) * 5;
+    const x = round(Math.cos(azimuthRad) * (scale * elevation * easeOffset));
+    const y = round(Math.sin(azimuthRad) * (scale * elevation * easeOffset));
 
-			// radius/ shadow blur
-			const easeRadius = easeQuadOut(normalize(i, 0, numShadows))
-			const radius = round(
-				SHADOW_BASE_BLUR *
-					increaseRadiusWithDistance *
-					easeRadius *
-					(elevation * 2)
-			)
+    // radius/ shadow blur
+    const easeRadius = easeQuadOut(normalize(i, 0, numShadows));
+    const radius = round(SHADOW_BASE_BLUR * increaseRadiusWithDistance * easeRadius * (elevation * 2));
 
-			return {
-				type,
-				blendMode: 'NORMAL',
-				visible: true,
-				color: { ...color, a },
-				offset: {
-					x,
-					y
-				},
-				radius,
-				spread: 0
-			}
-		}
-	)
-	return shadows
+    return {
+      type,
+      blendMode: "NORMAL",
+      visible: true,
+      color: { ...color, a },
+      offset: {
+        x,
+        y,
+      },
+      radius,
+      spread: 0,
+    };
+  });
+  // due to the way the easing function works, we'll always end up with an almost transparent 0px 0px 0px 0px shadow. we'll just filter that one out...
+  let _shadows: any[] = shadows;
+  let filtered = _shadows.filter((shadow) => !(shadow.offset.x === 0 && shadow.offset.y === 0 && shadow.radius === 0));
+  return filtered as DropShadowEffect[] | InnerShadowEffect[];
 }
