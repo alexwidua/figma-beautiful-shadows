d4ce0e7b71cd95c78207c1a71f48977ce7cbb7d3
Alex Widua <a.widua@gmail.com>
2023-09-21 16:21:53 +0200

    bump version

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import { Preview } from "./store/createPreview";
 import { Selection } from "./store/createSelection";
 import { PreviewBounds } from "./hooks/usePreviewBounds";
 
-const VERSION = 15;
+const VERSION = 16;
 const PLUGIN_DATA_KEY = "beautiful_shadow";
 const VALID_NODE_TYPES: Array<NodeType> = [
   "BOOLEAN_OPERATION",
